03190d7ded2740a11642f8ea99fdd0b04f512b46
cerqueirav <victorcerx@gmail.com>
2021-12-09 03:05:56 -0300

    my portfolio v1

---
+++ assets/js/owl/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:3
+        },
+        1200:{
+            items: 3
+        }
+    }
+})
